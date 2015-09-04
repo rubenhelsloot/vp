df3e281c49b2319010a5723101cdbc6b1abdd765
rht670 <rht670@student.vu.nl>
2015-09-04 17:29:36 +0200

    Started on javadoc

---
+++ Assignment 1/assignment1/IdentifierInterface.java
@@ -0,0 +1,18 @@
+package assignment1;
+
+/* ADT for the class Identifier
+ * 
+ * @author Ruben Helsloot & Sherida van den Bent
+ * 
+ * @elements
+ * @structure
+ * @domain
+ * 
+ * @constructor
+ * 
+ */
+
+public interface IdentifierInterface {
+	
+	void init(char c);
+}
