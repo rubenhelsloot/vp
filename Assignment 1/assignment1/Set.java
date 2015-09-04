db82b275adcd1cb183beb706cb59e25b30f0fd46
rht670 <rht670@student.vu.nl>
2015-09-04 21:54:41 +0200

    Built Set interface and generated Set

---
+++ Assignment 1/assignment1/Set.java
@@ -0,0 +1,59 @@
+package assignment1;
+
+class Set implements SetInterface {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addElement(Identifier id) throws ArrayIndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeElement(Identifier id) throws ArrayIndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Set union(Set S) throws ArrayIndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set difference(Set S) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set intersection(Set S) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set symmetricDifference(Set S) throws ArrayIndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
