4096053ac27389f89aee2f2b7db91670140d097d
rht670 <rht670@student.vu.nl>
2015-09-10 11:46:34 +0200

    This shit is awesome!

--- Assignment 1/assignment1/Main.java
+++ Assignment 1/assignment1/Main.java
@@ -1,13 +1,14 @@
 package assignment1;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
 	public static final int MAX_INPUT_SIZE = 10;
-	public static String DEFAULT_DELIMITER = " ", QUESTION_ONE = "Give the first set: ",
+	public static final String DEFAULT_DELIMITER = "", QUESTION_ONE = "Give the first set: ",
 			QUESTION_TWO = "Give the second set: ";
-	public static char OPEN = '{', CLOSE = '}', SPACE = ' ', BETWEENER = ' ';
+	public static final char OPEN = '{', CLOSE = '}', SPACE = ' ', BETWEENER = ' ';
 
 	private Scanner in;
 
@@ -20,39 +21,46 @@ public class Main {
 		return in.next().charAt(0);
 	}
 
-	private boolean nextCharIsSpecial(Scanner in, char c) {
-		return in.hasNext(c + "");
+//	private boolean hasNextCharIsSpecial(Scanner in, char c) {
+//		String s = Character.toString(c);
+//		System.out.println(s.equals(in.next()));
+//		return in.hasNext(s);
+//	}
+	
+	private boolean hasNextCharIsSpecial(Scanner in, char c) {
+		return in.hasNext(Pattern.quote(c+""));
 	}
 
-	private boolean nextCharIsDigit(Scanner in) {
+	private boolean hasNextCharIsDigit(Scanner in) {
 		return in.hasNext("[0-9]");
 	}
 
-	private boolean nextCharIsLetter(Scanner in) {
+	private boolean hasNextCharIsLetter(Scanner in) {
 		return in.hasNext("[a-zA-Z]");
 	}
 
-	private boolean nextCharIsAlphaNumerical(Scanner in) {
-		return (nextCharIsDigit(in) || nextCharIsLetter(in));
+	private boolean hasNextCharIsAlphaNumerical(Scanner in) {
+		return (hasNextCharIsDigit(in) || hasNextCharIsLetter(in));
 	}
 
-	private boolean nextCharIsSpace(Scanner in) {
+	private boolean hasNextCharIsSpace(Scanner in) {
 		return in.hasNext(String.valueOf(SPACE));
 	}
 
-	private boolean nextCharIsNewLine(Scanner in) {
-		return nextCharIsSpecial(in, '\n') || nextCharIsSpecial(in, '\r');
+	private boolean hasNextCharIsNewLine(Scanner in) {
+		return hasNextCharIsSpecial(in, '\n') || hasNextCharIsSpecial(in, '\r');
 	}
 
 	private void removeWhiteSpace(Scanner in) {
-		while (nextCharIsSpace(in)) {
+		while (hasNextCharIsSpace(in)) {
 			in.next(); // Pass over the whitespace.
 		}
 	}
 
 	private boolean firstElement(Scanner in) {
-		if (!nextCharIsSpecial(in, OPEN)) {
-			if (nextCharIsNewLine(in)) {
+		if (!hasNextCharIsSpecial(in, OPEN)) {
+			System.out.println("We are now here.");
+			if (hasNextCharIsNewLine(in)) {
 				System.out.println("New line");
 				in.nextLine();
 				return false;
@@ -63,81 +71,88 @@ public class Main {
 		return true;
 	}
 
-//	private boolean parseInput(Set answer, Scanner in) {
-//		if (!nextCharIsLetter(in)) {
-//			System.out.println("The identifier should start with a letter.");
-//			in.nextLine();
-//			return false;
-//		}
-//
-//		Identifier id = new Identifier();
-//		id.init(nextChar(in));
-//		while (nextCharIsAlphaNumerical(in)) {
-//			id = id.add(nextChar(in));
-//		}
-//
-//		if (nextCharIsNewLine(in)) {
-//			System.out.println("The input should end with the closing tag, " + CLOSE + ".");
-//			in.nextLine();
-//			return false;
-//		}
-//
-//		if (nextCharIsSpecial(in, CLOSE) || nextCharIsSpecial(in, DEFAULT_DELIMITER.charAt(0))) {
-//			System.out.println("The input can only consists of numbers and letters, a closing tag " + CLOSE
-//					+ " and an opening tag " + OPEN + ".");
-//			in.nextLine();
-//			return false;
-//		}
-//
-//		answer.addElement(id);
-//
-//		if (answer.getSize() > MAX_INPUT_SIZE) {
-//			System.out.println("The input contains more than the maximum of " + MAX_INPUT_SIZE + " elements.");
-//			in.nextLine();
-//			return false;
-//		}
-//		removeWhiteSpace(in);
-//
-//		return true;
-//	}
+	// private boolean parseInput(Set answer, Scanner in) {
+	// if (!hasNextCharIsLetter(in)) {
+	// System.out.println("The identifier should start with a letter.");
+	// in.nextLine();
+	// return false;
+	// }
+	//
+	// Identifier id = new Identifier();
+	// id.init(nextChar(in));
+	// while (hasNextCharIsAlphaNumerical(in)) {
+	// id = id.add(nextChar(in));
+	// }
+	//
+	// if (hasNextCharIsNewLine(in)) {
+	// System.out.println("The input should end with the closing tag, " + CLOSE
+	// + ".");
+	// in.nextLine();
+	// return false;
+	// }
+	//
+	// if (hasNextCharIsSpecial(in, CLOSE) || hasNextCharIsSpecial(in,
+	// DEFAULT_DELIMITER.charAt(0))) {
+	// System.out.println("The input can only consists of numbers and letters, a
+	// closing tag " + CLOSE
+	// + " and an opening tag " + OPEN + ".");
+	// in.nextLine();
+	// return false;
+	// }
+	//
+	// answer.addElement(id);
+	//
+	// if (answer.getSize() > MAX_INPUT_SIZE) {
+	// System.out.println("The input contains more than the maximum of " +
+	// MAX_INPUT_SIZE + " elements.");
+	// in.nextLine();
+	// return false;
+	// }
+	// removeWhiteSpace(in);
+	//
+	// return true;
+	// }
 
 	private boolean readInput(Set answer, Scanner in) {
 		removeWhiteSpace(in);
 
-		if (firstElement(in)) {
-			System.out.println("Is first");
+		if (!firstElement(in)) {
 			return false;
+		} else {
+			in.next();
 		}
-
-		in.next();
+		
 		removeWhiteSpace(in);
 
-		while (!nextCharIsSpecial(in, CLOSE)) {
-			if (!nextCharIsLetter(in)) {
+		while (!hasNextCharIsSpecial(in, CLOSE)) {
+			if (!hasNextCharIsLetter(in)) {
 				System.out.println("The identifier should start with a letter.");
 				in.nextLine();
 				return false;
 			}
-
+			
 			Identifier id = new Identifier();
 			id.init(nextChar(in));
-			while (nextCharIsAlphaNumerical(in)) {
+			while (hasNextCharIsAlphaNumerical(in)) {
 				id = id.add(nextChar(in));
+				System.out.println(id.sb);
 			}
 
-			if (nextCharIsNewLine(in)) {
+			if (hasNextCharIsNewLine(in)) {
 				System.out.println("The input should end with the closing tag, " + CLOSE + ".");
 				in.nextLine();
 				return false;
 			}
-
-			if (nextCharIsSpecial(in, CLOSE) || nextCharIsSpecial(in, DEFAULT_DELIMITER.charAt(0))) {
-				System.out.println("The input can only consists of numbers and letters, a closing tag " + CLOSE
-						+ " and an opening tag " + OPEN + ".");
-				in.nextLine();
-				return false;
-			}
-
+			
+//			if (hasNextCharIsSpecial(in, CLOSE) || hasNextCharIsSpecial(in, SPACE)) {
+//				System.out.println("The truth? YOU CAN\'T HANDLE THE TRUTH! ");
+//				System.out.println("The input can only consists of numbers and letters, a closing tag " + CLOSE
+//						+ " and an opening tag " + OPEN + ".");
+//				in.nextLine();
+//				return false;
+//			}
+			
+			System.out.println(id.sb);
 			answer.addElement(id);
 
 			if (answer.getSize() > MAX_INPUT_SIZE) {
@@ -147,15 +162,16 @@ public class Main {
 			}
 			removeWhiteSpace(in);
 		}
-		
+
 		in.next();
 		removeWhiteSpace(in);
-		if(nextCharIsNewLine(in)) {
+		if (hasNextCharIsNewLine(in)) {
 			in.nextLine();
 			return true;
 		}
-		
-		System.out.println("There are characters outside of the domain, please let " + CLOSE + " be the last element in your input.");
+
+		System.out.println("There are characters outside of the domain, please let " + CLOSE
+				+ " be the last element in your input.");
 		in.nextLine();
 		return false;
 	}
@@ -184,7 +200,7 @@ public class Main {
 
 		first.init();
 		second.init();
-		
+
 		while (input(first, second)) {
 			System.out.println("HEWWWWROO");
 		}
